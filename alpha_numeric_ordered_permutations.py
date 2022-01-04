c0cd6165a6a06031559aaebac65dde4c196ce43c
Philip Gundy <philipgundy@gmail.com>
2022-01-03 22:00:06 -0800

    input reliability improvements

--- alpha_numeric_ordered_permutations.py
+++ alpha_numeric_ordered_permutations.py
@@ -1,18 +1,21 @@
 #%% packages
 print("required packages: numpy and itertools")
-import numpy as np
+
 import itertools
 
+import numpy as np
+
+
 #%%
-
-
-def alpha_numeric_ordered_permutation(input_list, permutation_depth_max=None):
+def alpha_numeric_ordered_permutation(
+    input_list: list, depth_max: int = None
+):
     """
     input_list -- np.array, index, list, etc.
-    permutation_depth_max -- The maximum length of the permutations. Default is len(input_list).
+    depth_max -- The maximum length of the permutations. Default is len(input_list).
     
     Example: 
-        alpha_numeric_ordered_permutations(input_list = list(["a", "b", "c"]), permutation_depth_max=2)
+        alpha_numeric_ordered_permutations(input_list = list(["a", "b", "c"]), depth_max=2)
         would return: [[a], [b], [a,b], [a,c], [b,c]]
         
         
@@ -22,23 +25,25 @@ def alpha_numeric_ordered_permutation(input_list, permutation_depth_max=None):
     permutation_interim = []
     permutation_final = []
 
-    if permutation_depth_max is None:
-        permutation_depth_max = len(input_list)
+    if depth_max is None:
+        depth_max = len(input_list)
 
-    if permutation_depth_max > 7:
+    if depth_max > 7:
         print(
-            "Having a permutation_depth_max of",
-            permutation_depth_max,
+            "Having a depth_max of",
+            depth_max,
             "will take several minutes to compute.",
-            "\nIt is suggested to keep permutation_depth_max under 7.",
+            "\nIt is suggested to keep depth_max under 7.",
         )
 
-    for i in np.arange(0, permutation_depth_max, 1) + 1:
+    for i in np.arange(0, depth_max, 1) + 1:
         permutations = list(itertools.permutations(input_list, int(i)))
         permutations = list(map(list, permutations))
         permutations = list(map(sorted, permutations))
         ## Convert our sorted sub-lists into unique tuples using set() & then back to lists
-        permutations = [list(x) for x in set(tuple(x) for x in permutations)]
+        permutations = [
+            list(x) for x in set(tuple(x) for x in permutations)
+        ]
         permutations.sort()
         print(
             "Permutations with element length",
@@ -52,14 +57,16 @@ def alpha_numeric_ordered_permutation(input_list, permutation_depth_max=None):
         # permutation_interim.append(permutations)
         permutation_interim = [*permutation_interim, permutations]
 
-        if i == permutation_depth_max:
+        if i == depth_max:
             ## unnest the permutations
-            permutation_final = list(itertools.chain.from_iterable(permutation_interim))
+            permutation_final = list(
+                itertools.chain.from_iterable(permutation_interim)
+            )
             print(
                 "In total there are",
                 len(permutation_final),
-                "permutations with a permutation_depth_max max of",
-                permutation_depth_max,
+                "permutations with a depth_max max of",
+                depth_max,
             )
 
             permutation_final = list(filter(None, permutation_final))
@@ -74,7 +81,7 @@ if __name__ == "__main__":
 
     print("\n\n\nNow for test_list1\n\n")
     test1 = alpha_numeric_ordered_permutation(
-        input_list=test_list1, permutation_depth_max=2
+        input_list=test_list1, depth_max=2
     )
 
     ##TODO: understand why pylance errors out on example 2: "Object of type "None" cannot be used as iterable value"
@@ -90,7 +97,7 @@ if __name__ == "__main__":
 
     print("\n\n\nNow for test_list2\n\n")
     test2 = alpha_numeric_ordered_permutation(
-        input_list=test_list2, permutation_depth_max=2
+        input_list=test_list2, depth_max=2
     )
 
     for i in list(filter(None, test2)):
